2b74b02175ddf9c01889b3ac140092ae05512743
WagnerRodrigues181 <rodrigueswagner181@gmail.com>
2025-04-29 11:03:37 -0300

    fix: cria componente de erro 404 e corrige bug de permanência da mensagem

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,17 +2,28 @@ import { UserProps } from "../types/user";
 
 import Search from "../components/Search";
 import User from "../components/User";
+import Error from "../components/Error";
 
 import { useState } from "react";
 
 const Home = () => {
   // Inicia o user como nulo para saber quando eu tenho um usuário armazenado aqui.
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loadUser = async (userName: string) => {
+    // Consertando o bug da permanência da mensagem de erro.
+    setError(false);
+    setUser(null);
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -31,6 +42,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
